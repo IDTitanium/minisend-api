2ef039a2f70fd2987ac5172470798ece58ca553d
idris lawal <idriseun222@gmail.com>
2022-06-10 11:23:49 +0000

    added more test

--- tests/Feature/EmailNotificationTest.php
+++ tests/Feature/EmailNotificationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Jobs\SendEmail;
+use App\Mail\GenericMailer;
 use App\Models\EmailNotification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,6 +11,7 @@ use Illuminate\Http\Response;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Http\File;
+use Illuminate\Support\Facades\Mail;
 
 class EmailNotificationTest extends TestCase
 {
@@ -136,6 +138,34 @@ class EmailNotificationTest extends TestCase
                     'body' => $data['body']
                 ]
         ]);
+    }
+
+    public function testCanGetEmailNotificationStats() {
+        Mail::fake();
+        $attributes = [
+            'from' => 'xyz@gmail.com',
+            'from_name' => 'idris',
+            'to' => 'abc@gmail.com',
+            'subject' => 'a test',
+            'body' => 'test body',
+            'attachments' => [new File(base_path('tests/resources/testFile.txt'))]
+        ];
+        $response = $this->post('/api/emails', $attributes);
+        $response->assertStatus(Response::HTTP_CREATED);
+        $response->assertSeeText('Email posted successfully');
+
+        $data = $response->decodeResponseJson();
+        $data = $data['data'];
+
+        $response = $this->get('/api/emails/stats');
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertSeeText('Stats fetched successfully');
+        $data = $response->decodeResponseJson();
+        $data = $data['data'];
+        $this->assertEquals(0, $data['countPosted']);
+        $this->assertEquals(1, $data['countSent']);
+        $this->assertEquals(0, $data['countFailed']);
+        Mail::assertSent(GenericMailer::class);
     }
 
 }
